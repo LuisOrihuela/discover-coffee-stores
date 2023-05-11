799e070fce93a60aa72f360cfa42fd8d508d10c4
Luis Alberto Orihuela Quintos <luis@MacBook-Pro-de-Luis.local>
2023-05-11 14:00:01 -0500

    feat: improving coffee store styling

--- src/pages/coffee-store/[id].jsx
+++ src/pages/coffee-store/[id].jsx
@@ -1,18 +1,99 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
+import coffeeStores from "../../data/coffee-stores.json";
+import Head from "next/head";
+import styles from "@/styles/coffee-store.module.css";
+import Image from "next/image";
+import cls from "classnames";
 
-const CoffeeStore = () => {
+// this paths will determine what pages will be generated at build time
+export async function getStaticPaths() {
+  const paths = coffeeStores.map(({ id }) => ({
+    params: { id: id.toString() },
+  }));
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+// the props that our static pages will get when they get generated at build time
+export async function getStaticProps({ params }) {
+  const { id } = params;
+
+  const coffeeStore = coffeeStores.find(
+    (coffeeStore) => coffeeStore.id === Number(id)
+  );
+
+  return {
+    props: {
+      coffeeStore,
+    },
+  };
+}
+
+const CoffeeStore = ({ coffeeStore }) => {
   const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+  const { address, imgUrl, name, neighbourhood } = coffeeStore;
+
+  const handleUpVoteButton = () => {
+    console.log("hanlde up vote!");
+  };
+
   return (
     <div>
-      coffee store page {router.query.id}
-      <Link href="/">
-        Back to home
-      </Link>
-      <Link href='/coffee-store/asdf'>
-        To dynamic page
-      </Link>
+      <Head>
+        <title>{name}</title>
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href="/">Back to home</Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+          <Image
+            src={imgUrl}
+            width={600}
+            height={360}
+            className={styles.storeImg}
+            alt={name}
+          />
+        </div>
+        <div className={cls("glass", styles.col2)}>
+          <div className={styles.iconWrapper}>
+            <Image
+              src="/static/icons/places.svg"
+              width={24}
+              height={24}
+              alt=""
+            />
+            <p className={styles.text}>{address}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image
+              src="/static/icons/nearMe.svg"
+              width={24}
+              height={24}
+              alt=""
+            />
+            <p className={styles.text}>{neighbourhood}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/star.svg" width={24} height={24} alt="" />
+            <p className={styles.text}>1</p>
+          </div>
+          <button className={styles.upvoteButton} onClick={handleUpVoteButton}>
+            Up vote!
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,16 +4,20 @@ import { IBM_Plex_Sans } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Banner from "@/components/Banner/banner";
 import Card from "@/components/Card/Card";
-import coffeeStores from "@/data/coffee-stores.json"
+import coffeeStores from "@/data/coffee-stores.json";
 
-const ibmPlexSans = IBM_Plex_Sans({ weight: ['400', '500', '700'], subsets: ['latin'], style: 'normal' })
+const ibmPlexSans = IBM_Plex_Sans({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+  style: "normal",
+});
 
-export async function getStaticProps(context) {
+export async function getStaticProps() {
   return {
     props: {
-      coffeeStores
-    }
-  }
+      coffeeStores,
+    },
+  };
 }
 
 export default function Home({ coffeeStores }) {
@@ -28,7 +32,7 @@ export default function Home({ coffeeStores }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={[ibmPlexSans.className, styles.main].join(' ')}>
+      <main className={[ibmPlexSans.className, styles.main].join(" ")}>
         <Banner
           buttonText="View stores nearby"
           handleOnClick={handleOnBannerBtnClick}
@@ -41,13 +45,22 @@ export default function Home({ coffeeStores }) {
             height={400}
           />
         </div>
-        <div className={styles.cardLayout}>
-          {
-            coffeeStores.map(({ name, id, imgUrl }) => (
-              <Card key={id} className={styles.card} name={name} imgUrl={imgUrl} href={`/coffee-store/${id}`} />
-            ))
-          }
-        </div>
+        {coffeeStores.length > 0 && (
+          <>
+            <h2 className={styles.heading2}>Toronto stores</h2>
+            <div className={styles.cardLayout}>
+              {coffeeStores.map(({ name, id, imgUrl }) => (
+                <Card
+                  key={id}
+                  className={styles.card}
+                  name={name}
+                  imgUrl={imgUrl}
+                  href={`/coffee-store/${id}`}
+                />
+              ))}
+            </div>
+          </>
+        )}
       </main>
     </>
   );
